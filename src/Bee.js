afa5e5f1eeff670593c1bdf324371bc3af4a171d
Kavita Poduri <kavi.pdri@gmail.com>
2021-11-15 10:28:11 -0800

    implemented all bees in prototypal style

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,8 @@
 var Bee = function() {
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
 
+Bee.prototype = new Grub();
+Bee.prototype.constructor = Bee;
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,16 @@
 var HoneyMakerBee = function() {
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = new Bee();
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+HoneyMakerBee.prototype.makeHoney = function () {
+  this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function () {
+  this.honeyPot--;
+};
